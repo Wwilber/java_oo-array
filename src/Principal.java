cc392d6c2add153f54cd9e37522690ffbf5b2a34
Wilber Ribeiro <wilber.programacao@gmail.com>
2026-08-05 15:33:42 -0300

    aula 0563 -  08.02 - Acessando e atribuindo elementos de arrays

---
+++ src/Principal.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Principal {
+    public static void main(String[] args) {
+
+        int[] notas = {8, 5, 4, 9, 10};
+
+        System.out.println(notas[0]);
+        System.out.println(notas[3]);
+        System.out.println(notas[4]);
+
+        int posicao = 3;
+
+        System.out.println(notas[0]);
+        System.out.println(notas[posicao-1]);
+
+
+    }
+}
