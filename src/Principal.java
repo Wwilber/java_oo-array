314138a80d58702f28164cf6c98e83571ccb34be
wilber ribeiro <wilber.programacao@gmail.com>
2026-08-08 15:12:03 -0300

    AULA #F0566 08_05_Ordenando arrays em ordem natural e reversa.

--- src/Principal.java
+++ src/Principal.java
@@ -1,19 +1,20 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Principal {
     public static void main(String[] args) {
 
+        // ORDENAMENTO DO PRÓPRIO ARRAY (notas):
         int[] notas = {8, 5, 4, 9, 10};
+        Arrays.sort(notas);
+        System.out.println(Arrays.toString(notas));
 
-        // TRNASFORMAR ARRAY EM STRING: classe utilitária para trabalhar com Arrays: possui métodos estáticos:
-
-        String notasEmString = Arrays.toString(notas);
-
-
-        System.out.println(notasEmString);
-
+        // REVERTER O ORDENAMENTO DO ARRAY (notas2:
+        Integer[] notas2 = {28, 17, 12, 29, 44};
+        Arrays.sort(notas2, Comparator.reverseOrder());
+        System.out.println(Arrays.toString(notas2));
 
     }
 }
